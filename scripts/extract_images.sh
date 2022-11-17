c3b777daaea8285b665a56a056d30dfa14c26d25
José Guilherme Vanz <jguilhermevanz@suse.com>
2022-11-17 16:42:05 -0300

    Fix issues reported by shellcheck.
    Updates the scripts from the scripts directory fixing the issues
    reported by shellcheck.
    
    Signed-off-by: José Guilherme Vanz <jguilhermevanz@suse.com>

--- scripts/extract_images.sh
+++ scripts/extract_images.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
+set -euo pipefail
 
 CHART_DIR=$1
-CHARTS_DIRS=`find $CHART_DIR -type d -exec test -e '{}'/values.yaml \; -print`
-TMP_IMAGE_FILE=/tmp/imageslist.txt
+CHARTS_DIRS=$(find "$CHART_DIR" -type d -exec test -e '{}'/values.yaml \; -print)
+IMAGELIST_FILENAME=imagelist.txt
+TMP_IMAGE_FILE=/tmp/$IMAGELIST_FILENAME
 
-if [ -e imagelist.txt ]; then
-	rm imagelist.txt
+find "$CHART_DIR" -type f -name $IMAGELIST_FILENAME -delete
+if [ -e $IMAGELIST_FILENAME ]; then
+       rm $IMAGELIST_FILENAME
 fi
+
 for chart in $CHARTS_DIRS; do
-	helm template --values $chart/values.yaml $chart/ | yq -r "..|.image?" | sort -u | grep -v "null"  > $TMP_IMAGE_FILE
+	helm template --values "$chart"/values.yaml "$chart"/ | yq -r "..|.image?" | grep -v "null"  > $TMP_IMAGE_FILE
 	sed --in-place '/---/d' $TMP_IMAGE_FILE
-	cat $TMP_IMAGE_FILE | sort -u > $chart/imagelist.txt
-	# Unified imagelist.txt file. Containing image from all charts.
-	cat $chart/imagelist.txt >> imagelist.txt
+	mv $TMP_IMAGE_FILE "$chart"/$IMAGELIST_FILENAME 
 done
+
+# Delete the empty imagelist.txt files.
+find "$CHART_DIR" -type f -name $IMAGELIST_FILENAME -empty -delete
+find "$CHART_DIR" -type f -name $IMAGELIST_FILENAME -print0 | xargs --null cat > $TMP_IMAGE_FILE
+mv $TMP_IMAGE_FILE $IMAGELIST_FILENAME
+
+find "$CHART_DIR" -type f -name $IMAGELIST_FILENAME -exec sort -u -o \{\} \{\} \;
+sort -u -o $IMAGELIST_FILENAME $IMAGELIST_FILENAME
--- scripts/extract_policies.sh
+++ scripts/extract_policies.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 CHART_DIR="$1"
 CHARTS_DIRS=$(find "$CHART_DIR" -type d -exec test -e '{}'/values.yaml \; -print)
 POLICYLIST_FILENAME=policylist.txt
-TMP_IMAGE_FILE=/tmp/$POLICYLIST_FILENAME
+TMP_POLICY_FILE=/tmp/$POLICYLIST_FILENAME
 
 find "$CHART_DIR" -type f -name $POLICYLIST_FILENAME -delete
 if [ -e $POLICYLIST_FILENAME ]; then
@@ -14,24 +14,24 @@ fi
 for chart in $CHARTS_DIRS; do
 	if [[ $chart == *"-defaults" ]]; then
 		helm template --values "$chart"/values.yaml --set recommendedPolicies.enabled=true "$chart/" \
-			| yq -r ". | select(.kind==\"ClusterAdmissionPolicy\" or .kind==\"AdmissionPolicy\") | .spec.module" > "$TMP_IMAGE_FILE"
-		sed --in-place '/---/d' $TMP_IMAGE_FILE
+			| yq -r ". | select(.kind==\"ClusterAdmissionPolicy\" or .kind==\"AdmissionPolicy\") | .spec.module" > "$TMP_POLICY_FILE"
+		sed --in-place '/---/d' $TMP_POLICY_FILE
 		# adds the registry prefix if necessary
-		file=$(cat $TMP_IMAGE_FILE)
+		file=$(cat $TMP_POLICY_FILE)
 		for line in $file; do
-			if [[ $(echo $line | awk '!/(https:\/\/|registry:\/\/)/') ]]; then
-				echo $line | sed 's/^/registry:\/\//'  >> "$chart"/$POLICYLIST_FILENAME 
+			if [[ $(echo "$line" | awk '!/(https:\/\/|registry:\/\/)/') ]]; then
+				echo "$line" | sed 's/^/registry:\/\//'  >> "$chart"/$POLICYLIST_FILENAME 
 				continue
 			fi
-			echo $line >> "$chart"/$POLICYLIST_FILENAME 
+			echo "$line" >> "$chart"/$POLICYLIST_FILENAME 
 		done
 	fi
 done
 
 # Delete the empty policylist.txt files.
-find $CHART_DIR -type f -name $POLICYLIST_FILENAME -empty -delete
-# Unified imagelist.txt file. Containing image from all charts.
-find $CHART_DIR -type f -name $POLICYLIST_FILENAME | xargs cat > $POLICYLIST_FILENAME
+find "$CHART_DIR" -type f -name $POLICYLIST_FILENAME -empty -delete
+find "$CHART_DIR" -type f -name $POLICYLIST_FILENAME -print0 | xargs --null cat > $TMP_POLICY_FILE
+mv $TMP_POLICY_FILE $POLICYLIST_FILENAME
 # Sort policylist file
-find $CHART_DIR -type f -name $POLICYLIST_FILENAME -exec sort -u -o \{\} \{\} \;
+find "$CHART_DIR" -type f -name $POLICYLIST_FILENAME -exec sort -u -o \{\} \{\} \;
 sort -u -o $POLICYLIST_FILENAME $POLICYLIST_FILENAME
