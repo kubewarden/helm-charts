4cdcea3bdc955a8ca2aee100e9e78c7a44bc7a76
José Guilherme Vanz <jguilhermevanz@suse.com>
2023-03-13 16:39:18 -0300

    Kubewarden components changelog in the chart release.
    Configure Helm chart releaser to add links to the Kubewarden components
    (controller and policy server) in the Helm charts releases.
    
    Signed-off-by: José Guilherme Vanz <jguilhermevanz@suse.com>

---
+++ scripts/generate_changelog_files.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+
+CHART_DIR=$1
+IMAGELIST_FILENAME=$2
+TMP_CHANGELOG_FILE_PATH=/tmp/changelog.md
+
+CONTROLLER_VERSION=$(grep "kubewarden-controller" < "$IMAGELIST_FILENAME" | sed "s/.*kubewarden-controller:\(\)/\1/g")
+CONTROLLER_URL=$(gh release view "$CONTROLLER_VERSION" --repo kubewarden/kubewarden-controller --json "url" | jq -r ".url" )
+POLICY_SERVER_VERSION=$(grep "policy-server" < "$IMAGELIST_FILENAME" | sed "s/.*policy-server:\(\)/\1/g")
+POLICY_SERVER_URL=$(gh release view "$POLICY_SERVER_VERSION" --repo kubewarden/policy-server --json "url" | jq -r ".url" )
+
+echo "Kubewarden controller [changelog]($CONTROLLER_URL)" >> $TMP_CHANGELOG_FILE_PATH
+echo "Policy server [changelog]($POLICY_SERVER_URL)" >> $TMP_CHANGELOG_FILE_PATH
+cp $TMP_CHANGELOG_FILE_PATH "$CHART_DIR/kubewarden-controller/CHANGELOG.md"
+cp $TMP_CHANGELOG_FILE_PATH "$CHART_DIR/kubewarden-defaults/CHANGELOG.md"
+cp $TMP_CHANGELOG_FILE_PATH "$CHART_DIR/kubewarden-crds/CHANGELOG.md"
+
