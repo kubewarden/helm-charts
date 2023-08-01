efe82a7ac817464727579bb198b5164896a9ab80
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2023-08-01 11:53:31 +0200

    refactor: Make shellcheck happy
    https://www.shellcheck.net/wiki/SC2129
    
    Signed-off-by: Víctor Cuadrado Juan <vcuadradojuan@suse.de>

--- scripts/generate_changelog_files.sh
+++ scripts/generate_changelog_files.sh
@@ -11,10 +11,11 @@ POLICY_SERVER_VERSION=$(grep "policy-server" < "$IMAGELIST_FILENAME" | sed "s/.*
 POLICY_SERVER_URL=$(gh release view "$POLICY_SERVER_VERSION" --repo kubewarden/policy-server --json "url" | jq -r ".url" )
 AUDIT_SCANNER_VERSION=$(grep "audit-scanner" < "$IMAGELIST_FILENAME" | sed "s/.*audit-scanner:\(\)/\1/g")
 AUDIT_SCANNER_URL=$(gh release view "$AUDIT_SCANNER_VERSION" --repo kubewarden/audit-scanner --json "url" | jq -r ".url" )
-
-echo "Kubewarden controller [changelog]($CONTROLLER_URL)" >> $TMP_CHANGELOG_FILE_PATH
-echo "Policy server [changelog]($POLICY_SERVER_URL)" >> $TMP_CHANGELOG_FILE_PATH
-echo "Audit scanner [changelog]($AUDIT_SCANNER_URL)" >> $TMP_CHANGELOG_FILE_PATH
+{
+  echo "Kubewarden controller [changelog]($CONTROLLER_URL)"
+  echo "Policy server [changelog]($POLICY_SERVER_URL)"
+  echo "Audit scanner [changelog]($AUDIT_SCANNER_URL)"
+} >> $TMP_CHANGELOG_FILE_PATH
 cp $TMP_CHANGELOG_FILE_PATH "$CHART_DIR/kubewarden-controller/CHANGELOG.md"
 cp $TMP_CHANGELOG_FILE_PATH "$CHART_DIR/kubewarden-defaults/CHANGELOG.md"
 cp $TMP_CHANGELOG_FILE_PATH "$CHART_DIR/kubewarden-crds/CHANGELOG.md"
