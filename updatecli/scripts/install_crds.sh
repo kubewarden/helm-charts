6f9cc1333ccac9b1960ff8748cb79d19a4cbb5e4
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2023-09-27 13:31:56 +0200

    ci: Allow updatecli to perform partial update of CRDs
    As the update_crds GHA job, updatecli could also receive only CRD to
    update, not both.
    
    Signed-off-by: Víctor Cuadrado Juan <vcuadradojuan@suse.de>

--- updatecli/scripts/install_crds.sh
+++ updatecli/scripts/install_crds.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-tar -xf /tmp/crds-controller.tar.gz
-find . -maxdepth 1 -name "*_policyserver*" -exec  mv \{\} /tmp/helm-charts/charts/kubewarden-crds/templates/policyservers.yaml \;
-find . -maxdepth 1 -name "*_admissionpolicies*" -exec  mv \{\} /tmp/helm-charts/charts/kubewarden-crds/templates/admissionpolicies.yaml \;
-find . -maxdepth 1 -name "*_clusteradmissionpolicies*" -exec  mv \{\} /tmp/helm-charts/charts/kubewarden-crds/templates/clusteradmissionpolicies.yaml \;
+if [ -f "/tmp/crds-controller.tar.gz" ]; then
+	tar -xf /tmp/crds-controller.tar.gz
+	find . -maxdepth 1 -name "*_policyserver*" -exec mv \{\} /tmp/helm-charts/charts/kubewarden-crds/templates/policyservers.yaml \;
+	find . -maxdepth 1 -name "*_admissionpolicies*" -exec mv \{\} /tmp/helm-charts/charts/kubewarden-crds/templates/admissionpolicies.yaml \;
+	find . -maxdepth 1 -name "*_clusteradmissionpolicies*" -exec mv \{\} /tmp/helm-charts/charts/kubewarden-crds/templates/clusteradmissionpolicies.yaml \;
+fi
 
-tar -xvf /tmp/crds-audit-scanner.tar.gz
-find . -maxdepth 1 -name "*_clusterpolicyreports*" -exec  mv \{\} charts/kubewarden-crds/templates/clusterpolicyreports.yaml \;
-find . -maxdepth 1 -name "*_policyreports*" -exec  mv \{\} charts/kubewarden-crds/templates/policyreports.yaml \;
-# add the if statement to allow users to skip the reports CRDs installation
-sed -i '1 i {{- if or .Values.installPolicyReportCRDs (not (hasKey .Values "installPolicyReportCRDs")) }}' charts/kubewarden-crds/templates/clusterpolicyreports.yaml
-sed -i '$ a {{ end }}' charts/kubewarden-crds/templates/clusterpolicyreports.yaml
-sed -i '1 i {{- if or .Values.installPolicyReportCRDs (not (hasKey .Values "installPolicyReportCRDs")) }}' charts/kubewarden-crds/templates/policyreports.yaml
-sed -i '$ a {{ end }}' charts/kubewarden-crds/templates/policyreports.yaml
+if [ -f "/tmp/crds-audit-scanner.tar.gz" ]; then
+	tar -xvf /tmp/crds-audit-scanner.tar.gz
+	find . -maxdepth 1 -name "*_clusterpolicyreports*" -exec mv \{\} charts/kubewarden-crds/templates/clusterpolicyreports.yaml \;
+	find . -maxdepth 1 -name "*_policyreports*" -exec mv \{\} charts/kubewarden-crds/templates/policyreports.yaml \;
+	# add the if statement to allow users to skip the reports CRDs installation
+	sed -i '1 i {{- if or .Values.installPolicyReportCRDs (not (hasKey .Values "installPolicyReportCRDs")) }}' charts/kubewarden-crds/templates/clusterpolicyreports.yaml
+	sed -i '$ a {{ end }}' charts/kubewarden-crds/templates/clusterpolicyreports.yaml
+	sed -i '1 i {{- if or .Values.installPolicyReportCRDs (not (hasKey .Values "installPolicyReportCRDs")) }}' charts/kubewarden-crds/templates/policyreports.yaml
+	sed -i '$ a {{ end }}' charts/kubewarden-crds/templates/policyreports.yaml
+fi
 
 # updatecli expects something in stdout when a change happened.
 echo "Changed!"
