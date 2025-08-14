3ec6c0cfbbe9f64fdf489f9c9b505c60e04615a1
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2025-08-14 13:04:32 +0200

    ci: Update check-common-values.sh script to work with yq
    We should target the flags of https://mikefarah.gitbook.io/yq
    
    not the flags of  https://kislyuk.github.io/yq/
    
    Signed-off-by: Víctor Cuadrado Juan <vcuadradojuan@suse.de>

--- scripts/check-common-values.sh
+++ scripts/check-common-values.sh
@@ -4,13 +4,13 @@ set -euo pipefail
 # Check that there's no divergence between ./common-values.yaml, key `global`,
 # and the `global` key on the helm-chart values.yaml
 
-diff <(yq --sort-keys .global common-values.yaml) <(yq --sort-keys .global charts/kubewarden-controller/values.yaml) || (
+diff <(yq eval '(.global // {}) | sort_keys(.)' common-values.yaml) <(yq eval '(.global // {}) | sort_keys(.)' charts/kubewarden-controller/values.yaml) || (
 	echo
-	echo "kubewaden-controller values.yaml diverges from common-values.yaml"
+	echo "kubewarden-controller values.yaml diverges from common-values.yaml"
 	exit 1
 )
-diff <(yq --sort-keys .global common-values.yaml) <(yq --sort-keys .global charts/kubewarden-defaults/values.yaml) || (
+diff <(yq eval '(.global // {}) | sort_keys(.)' common-values.yaml) <(yq eval '(.global // {}) | sort_keys(.)' charts/kubewarden-defaults/values.yaml) || (
 	echo
-	echo "kubewaden-defaults values.yaml diverges from common-values.yaml"
+	echo "kubewarden-defaults values.yaml diverges from common-values.yaml"
 	exit 1
 )
