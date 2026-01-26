4147ec2b5236048ac981326bcb7378968f351d31
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2026-01-26 16:00:02 +0100

    ci: Generate charts CHANGELOG.md from monorepo only
    Before, we were obtaining the changelogs from the different several
    repos.
    
    Now, we only need the changelog from the monorepo at
    `kubewarden/kubewarden-controller`.
    
    This fixes the chart release job, as we call
    `make generate-changelog-files` on release.
    
    Signed-off-by: Víctor Cuadrado Juan <vcuadradojuan@suse.de>

--- scripts/generate_changelog_files.sh
+++ scripts/generate_changelog_files.sh
@@ -5,18 +5,11 @@ CHART_DIR=$1
 IMAGELIST_FILENAME=$2
 TMP_CHANGELOG_FILE_PATH=/tmp/changelog.md
 
-CONTROLLER_VERSION=$(grep "kubewarden-controller" < "$IMAGELIST_FILENAME" | sed "s/.*kubewarden-controller:\(\)/\1/g")
-CONTROLLER_URL=$(gh release view "$CONTROLLER_VERSION" --repo kubewarden/kubewarden-controller --json "url" | jq -r ".url" )
-POLICY_SERVER_VERSION=$(grep "policy-server" < "$IMAGELIST_FILENAME" | sed "s/.*policy-server:\(\)/\1/g")
-POLICY_SERVER_URL=$(gh release view "$POLICY_SERVER_VERSION" --repo kubewarden/policy-server --json "url" | jq -r ".url" )
-AUDIT_SCANNER_VERSION=$(grep "audit-scanner" < "$IMAGELIST_FILENAME" | sed "s/.*audit-scanner:\(\)/\1/g")
-AUDIT_SCANNER_URL=$(gh release view "$AUDIT_SCANNER_VERSION" --repo kubewarden/audit-scanner --json "url" | jq -r ".url" )
+CONTROLLER_VERSION=$(grep "kubewarden-controller" <"$IMAGELIST_FILENAME" | sed "s/.*kubewarden-controller:\(\)/\1/g")
+CONTROLLER_URL=$(gh release view "$CONTROLLER_VERSION" --repo kubewarden/kubewarden-controller --json "url" | jq -r ".url")
 {
-  echo "Kubewarden controller [changelog]($CONTROLLER_URL)"
-  echo "Policy server [changelog]($POLICY_SERVER_URL)"
-  echo "Audit scanner [changelog]($AUDIT_SCANNER_URL)"
-} >> $TMP_CHANGELOG_FILE_PATH
+  echo "Kubewarden Admission Controller [changelog]($CONTROLLER_URL)"
+} >>$TMP_CHANGELOG_FILE_PATH
 cp $TMP_CHANGELOG_FILE_PATH "$CHART_DIR/kubewarden-controller/CHANGELOG.md"
 cp $TMP_CHANGELOG_FILE_PATH "$CHART_DIR/kubewarden-defaults/CHANGELOG.md"
 cp $TMP_CHANGELOG_FILE_PATH "$CHART_DIR/kubewarden-crds/CHANGELOG.md"
-
